f51158d94a531ff39d35c6d03954aa4e7b528bc8
madhusportIT <madhusudan.panigrahi@sportsit.net>
2024-03-18 16:58:16 +0530

    Initial commit

--- app/components/object-detection.js
+++ app/components/object-detection.js
@@ -7,66 +7,70 @@ import { renderPredictions } from "@/utils/render-predictions";
 
 let detectInterval;
 const ObjectDetection = () => {
-    const [loading, setIsLoading] = useState(false);
-    const webcamRef = React.useRef(null);
-    const canvasRef = React.useRef(null);
-    const showMyVideo = () => {
-        if (webcamRef.current !== null && webcamRef.current?.readyState === 4) {
-            console.log(webcamRef.current.getScreenshot());
-            const myVideoWidth = webcamRef.current.video.videoHeight;
-            const myVideoHeight = webcamRef.current.video.videoHeight;
+  const [loading, setIsLoading] = useState(false);
+  const webcamRef = React.useRef(null);
+  const canvasRef = React.useRef(null);
+  const showMyVideo = () => {
+    if (webcamRef.current !== null && webcamRef.current?.readyState === 4) {
+      console.log(webcamRef.current.getScreenshot());
+      const myVideoWidth = webcamRef.current.video.videoHeight;
+      const myVideoHeight = webcamRef.current.video.videoHeight;
 
-            webcamRef.current.video.width = myVideoWidth;
-            webcamRef.current.video.height = myVideoHeight;
-        }
-    };
-    async function runCoco() {
-        setIsLoading(true);
-        const net = await cocoSSDLoad();
-        setIsLoading(false);
-
-        detectInterval = setInterval(() => {
-            runObjectDetection(net);
-        }, 10);
+      webcamRef.current.video.width = myVideoWidth;
+      webcamRef.current.video.height = myVideoHeight;
     }
-    async function runObjectDetection(net) {
-        if (
-            canvasRef.current !== null &&
-            webcamRef.current !== null &&
-            webcamRef.current.video?.readyState === 4
-        ) {
-            canvasRef.current.height = webcamRef.current.video.videoHeight;
-            canvasRef.current.width = webcamRef.current.video.videoHeight;
+  };
+  async function runCoco() {
+    setIsLoading(true);
+    const net = await cocoSSDLoad();
+    setIsLoading(false);
 
-            const detectedObjets = await net.detect(webcamRef.current.video, undefined, 0.6)
-            // console.log("detectedObjets: ", detectedObjets);
-            const context = canvasRef.current.getContext("2d");
-            renderPredictions(detectedObjets, context);
-        }
+    detectInterval = setInterval(() => {
+      runObjectDetection(net);
+    }, 10);
+  }
+  async function runObjectDetection(net) {
+    if (
+      canvasRef.current !== null &&
+      webcamRef.current !== null &&
+      webcamRef.current.video?.readyState === 4
+    ) {
+      canvasRef.current.height = webcamRef.current.video.videoHeight;
+      canvasRef.current.width = webcamRef.current.video.videoHeight;
+
+      const detectedObjets = await net.detect(
+        webcamRef.current.video,
+        undefined,
+        0.6
+      );
+      // console.log("detectedObjets: ", detectedObjets);
+      const context = canvasRef.current.getContext("2d");
+      renderPredictions(detectedObjets, context);
     }
-    useEffect(() => {
-        runCoco();
-        showMyVideo();
-    }, []);
-    return (
-        <div className="mt-8">
-            {loading ? (
-                <div className="gradient-text">Loading AI Model</div>
-            ) : (
-                <div className="relative flex justify-center items-center gradient p-1.5 rounded-md ">
-                    <Webcam
-                        ref={webcamRef}
-                        className="lg:h-[720px] w-full rounded-md"
-                        muted
-                    />
-                    <canvas
-                        className="absolute top-0 left-0 z-99999 w-full lg:h-[720px]"
-                        ref={canvasRef}
-                    ></canvas>
-                </div>
-            )}
+  }
+  useEffect(() => {
+    runCoco();
+    showMyVideo();
+  }, []);
+  return (
+    <div className="mt-8">
+      {loading ? (
+        <div className="gradient-text">Loading AI Model</div>
+      ) : (
+        <div className="relative flex justify-center items-center gradient p-1.5 rounded-md ">
+          <Webcam
+            ref={webcamRef}
+            className="lg:h-[720px] w-full rounded-md"
+            muted
+          />
+          <canvas
+            className="absolute top-0 left-0 z-99999 w-full lg:h-[720px]"
+            ref={canvasRef}
+          ></canvas>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ObjectDetection;
--- utils/render-predictions.js
+++ utils/render-predictions.js
@@ -2,40 +2,40 @@
 // import throttle from 'lodash/throttle';
 
 export const renderPredictions = (predictions, ctx) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    // Fonts
-    const font = "16px sans-serif";
-    ctx.font = font;
-    ctx.textBaseline = "top";
+  // Fonts
+  const font = "16px sans-serif";
+  ctx.font = font;
+  ctx.textBaseline = "top";
 
-    predictions.forEach((prediction) => {
-        const [x, y, width, height] = prediction["bbox"];
+  predictions.forEach((prediction) => {
+    const [x, y, width, height] = prediction["bbox"];
 
-        const isPerson = prediction.class === "person";
+    const isPerson = prediction.class === "person";
 
-        // bounding box
-        ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
-        ctx.lineWidth = 4;
-        ctx.strokeRect(x, y, width, height);
+    // bounding box
+    ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
+    ctx.lineWidth = 4;
+    ctx.strokeRect(x, y, width, height);
 
-        // fill the color
-        ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`; // Set the fill color to red
-        ctx.fillRect(x, y, width, height);
+    // fill the color
+    ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`; // Set the fill color to red
+    ctx.fillRect(x, y, width, height);
 
-        // Draw the label background.
-        ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
-        const textWidth = ctx.measureText(prediction.class).width;
-        const textHeight = parseInt(font, 10); // base 10
-        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    // Draw the label background.
+    ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
+    const textWidth = ctx.measureText(prediction.class).width;
+    const textHeight = parseInt(font, 10); // base 10
+    ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
 
-        ctx.fillStyle = "#000000";
-        ctx.fillText(prediction.class, x, y);
+    ctx.fillStyle = "#000000";
+    ctx.fillText(prediction.class, x, y);
 
-        // if (isPerson) {
-        //     playAudio();
-        // }
-    });
+    // if (isPerson) {
+    //     playAudio();
+    // }
+  });
 };
 
 // const playAudio = throttle(() => {
