56b755cab08993d250ef47c1c8b6b9b81a05494d
madhusportIT <madhusudan.panigrahi@sportsit.net>
2024-03-19 11:05:52 +0530

    Sound Added by loadash

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import ObjectDetection from "./components/object-detection";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 p-8">
-      <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">Theif Detection Alaram</h1>
+      <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
+        Theif Detection Alaram
+      </h1>
       <ObjectDetection />
     </main>
   );
